868bc9532d5d5a76b5d2bc6a3c8b07aa4ed03eca
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-03-23 09:09:44 -0300

    Adicionada função para contar os episódios assistidos

--- controle-series/app/Models/Temporada.php
+++ controle-series/app/Models/Temporada.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Temporada extends Model
@@ -20,5 +21,12 @@ class Temporada extends Model
     public function serie()
     {
         return $this->belongsTo(Serie::class);
+    }
+
+    public function episodiosAssistidos(): Collection
+    {
+        return $this->episodios->filter(function (Episodio $episodio){
+            return $episodio->assistido;
+        });
     }
 }
--- controle-series/resources/views/temporadas/index.blade.php
+++ controle-series/resources/views/temporadas/index.blade.php
@@ -14,7 +14,7 @@
         @foreach ($temporadas as $temporada)
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 <a href="/temporadas/{{$temporada->id}}/episodios">Temporada {{$temporada->numero}}</a>
-                <span class="badge badge-secondary">0/{{$temporada->episodios->count()}}</span>
+                <span class="badge badge-secondary">{{$temporada->episodiosAssistidos()->count()}}/{{$temporada->episodios->count()}}</span>
             </li>
         @endforeach
     </ul>
