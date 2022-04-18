3be42eaf5143ac04c6c53c372be2d56609043402
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-04-18 09:28:00 -0300

    Alteração na classe TemporadaTest

--- tests/Unit/TemporadaTest.php
+++ tests/Unit/TemporadaTest.php
@@ -33,13 +33,71 @@ class TemporadaTest extends TestCase
         //Verificando os asserções
         $episodiosAssistidos = $temporada->episodiosAssistidos();
         $this->assertCount(3, $episodiosAssistidos);
-        foreach ($episodiosAssistidos as $episodio){
+        foreach ($episodiosAssistidos as $episodio) {
             $this->assertTrue($episodio->assistido);
         }
     }
 
-    public function testVerificarTodosEpisodiosAssistidosFalse(){
-        $this->assertTrue(true);
+    public function testVerificarTodosEpisodiosAssistidosFalse()
+    {
+        //instanciação das classes a serem utilizadas
+        $temporada = new Temporada();
+        $episodio1 = new Episodio();
+        $episodio2 = new Episodio();
+        $episodio3 = new Episodio();
+
+        //Adicionados valores para as variaveis
+        $episodio1->assistido = false;
+        $this->assertFalse($episodio1->assistido);
+        $episodio2->assistido = false;
+        $this->assertFalse($episodio2->assistido);
+        $episodio3->assistido = false;
+        $this->assertFalse($episodio3->assistido);
+
+        //Adicionados os episodios dentro da temporada
+        $temporada->episodios->add($episodio1);
+        $temporada->episodios->add($episodio2);
+        $temporada->episodios->add($episodio3);
+
+        //Verificando os asserções
+        $episodiosAssistidos = $temporada->episodiosAssistidos();
+        $this->assertCount(0, $episodiosAssistidos);
+        foreach ($episodiosAssistidos as $episodio) {
+            $this->assertFalse($episodio->assistido);
+        }
+    }
+
+    public function testeVerificarEpisodiosAssistidosTrueFalse()
+    {
+        //instanciação das classes a serem utilizadas
+        $temporada = new Temporada();
+        $episodio1 = new Episodio();
+        $episodio2 = new Episodio();
+        $episodio3 = new Episodio();
+
+        //Adicionados valores para as variaveis
+        $episodio1->assistido = false;
+        $this->assertFalse($episodio1->assistido);
+        $episodio2->assistido = true;
+        $this->assertTrue($episodio2->assistido);
+        $episodio3->assistido = false;
+        $this->assertFalse($episodio3->assistido);
+
+        //Adicionados os episodios dentro da temporada
+        $temporada->episodios->add($episodio1);
+        $temporada->episodios->add($episodio2);
+        $temporada->episodios->add($episodio3);
+
+        //Verificando os asserções
+        $episodiosAssistidos = $temporada->episodiosAssistidos();
+        $this->assertCount(1, $episodiosAssistidos);
+        foreach ($episodiosAssistidos as $episodio) {
+            if($episodio->assistido === true){
+                $this->assertTrue($episodio->assistido);
+            }else{
+                $this->assertFalse($episodio->assistido);
+            }
+        }
     }
 
 }
